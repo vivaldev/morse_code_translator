ff61c443b9504a59db59e142c9413351143c33ce
Valtteri Virtanen <valtteri.virtanen.dev@gmail.com>
2023-05-05 15:20:33 +0300

    Displays with new logic. Created morsesArray.

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,57 @@ import Header from "./components/Header";
 import MorseAbc from "./components/MorseAbc";
 import MorseBtn from "./components/MorseBtn";
 import ResultDisplay from "./components/ResultDisplay";
+import { morses } from "./data";
 
 interface AppTypes {
   longsignal: boolean;
   codeWord: string[];
   hasStarted: boolean;
-  timeoutId: NodeJS.Timeout | null;
   mouseDownTime: number;
+  pauseTimerId: NodeJS.Timeout | undefined;
+  morses: { letter: string; code: string };
 }
 
-const App: React.FC<AppTypes> = () => {
+const App: React.FC<AppTypes> = ({ morses }) => {
+  const [shortSignal, setShortSignal] = useState(false);
   const [longSignal, setLongSignal] = useState(false);
   const [codeWord, setCodeWord] = useState([""]);
   const [hasStarted, setHasStarted] = useState(false);
   const [mouseDownTime, setMouseDownTime] = useState(0);
+  const [pauseTimerId, setPauseTimerId] = useState(undefined);
 
-  const timeoutId = useRef(null);
+  const morsesArray = morses.map((morse) => ({
+    letter: morse.letter,
+    code: morse.code.split(""),
+  }));
+
+  const checkMorseCodeMatch = () => {
+    console.log("PAUSE");
+  };
 
   const handleMouseDown = () => {
     const timestamp = performance.now();
     setMouseDownTime(timestamp);
-    // console.log(`mouse down: ${timestamp}`);
   };
 
   const handleMouseUp = () => {
+    clearTimeout(pauseTimerId);
     const timestamp = performance.now();
     const timeDifference = timestamp - mouseDownTime;
-    // console.log(`mouse up: ${timestamp}`);
-    console.log(`time difference: ${timeDifference}`);
 
-    if (timeDifference > 200) {
-      console.log("long");
-    } else {
-      console.log("short");
+    if (timeDifference < 200) {
+      setCodeWord((word) => [...word, "."]);
+    }
+    if (timeDifference >= 200) {
+      setCodeWord((word) => [...word, "-"]);
     }
   };
+
+  // Clear any existing pause timer and set a new onMouseDown
+  clearTimeout(pauseTimerId);
+  const newPauseTimerId = setTimeout(() => {
+    checkMorseCodeMatch();
+  }, 2000);
 
   const handleStartClick = () => {
     return setHasStarted(true);
